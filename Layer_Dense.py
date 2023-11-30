235a061ab627f81ccb94b389b881f97ce3780729
Jared Orihuela Contreras <112034917+JaocHatter@users.noreply.github.com>
2023-11-29 21:49:57 -0500

    Delete Layer_Dense.py

--- Layer_Dense.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-class Layer_Dense:
-    def __init__(self,n_inputs,n_neurons):
-        self.weights = np.random.rand(n_inputs,n_neurons) #W
-        self.biases = np.zeros((1,n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs,self.weights) + self.biases # XW + b
